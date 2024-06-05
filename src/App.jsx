8b2a005d8bc6d8ddd68db5c9034c4ee44cb6f21d
Dezman Sykes <dSykes@Dezs-MBP.lan>
2024-06-05 13:43:42 -0500

    Added Not Found page, updated Navbar and Links

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout'
 import JobsPage from './pages/JobsPage'
+import NotFoundPage from './pages/NotFoundPage'
 const App = () => {
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Route>
   ))
   return <RouterProvider router={router} />;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import HeroCards from '../components/HeroCards'
 import JobListings from '../components/JobListings'
-import ViewAllJobs from '../ViewAllJobs'
+import ViewAllJobs from '../components/ViewAllJobs'
 
 
 const HomePage = () => {
